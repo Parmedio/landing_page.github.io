8801bcddf66b0aa4aadf6d476449a6a2da5bee5b
Parmedio <111775779+Parmedio@users.noreply.github.com>
2022-11-03 00:07:27 +0100

    dry script

--- script.js
+++ script.js
@@ -7,22 +7,19 @@ var col_text = document.getElementsByClassName("txt_inv");
 
 function page_edit(variable) {
     variable.addEventListener("input", function() {
-        backGstyle.style.background = "linear-gradient(" + Number(deg_grad.value) + "deg, " + but01.value + ", " + but02.value + ") no-repeat fixed";
-        css_param[0].textContent = "linear-gradient(" + Number(deg_grad.value) + "deg, " + but01.value + ", " + but02.value + ") no-repeat fixed";
-        col_text[0].style.color = but01.value;
-        col_text[1].style.color = but01.value;
-        col_text[3].style.color = but01.value;
-        col_text[4].style.color = but01.value;
+        a = "linear-gradient(" + Number(deg_grad.value) + "deg, " + but01.value + ", " + but02.value + ") no-repeat fixed";
+        backGstyle.style.background = a
+        css_param[0].textContent = a
+        var list_text = [0, 1, 3, 4];
+        function paint_all(iterable) {
+            for (const x of iterable) {
+                col_text[x].style.color = but01.value
+            }
+        }
+        paint_all(list_text)
     })
 }
 
 page_edit(but01);
 page_edit(but02);
 page_edit(deg_grad);
-
-//deg_grad.addEventListener("mouseenter", function() {console.log(deg_grad.value);})
-//but01.addEventListener("mouseenter", function() {console.log("sei passato sopra il colore 01");})
-//but02.addEventListener("mouseenter", function() {console.log("sei passato sopra il colore 02");})
-//css_param.addEventListener("mouseenter", function() {console.log("sei passato sopra la scheda parametri");})
-
-//onclick & oninput --- mettendo "oninput="nome_funzione(argomento)" tra gli attributi dell'elemento html saremmo riusciti ad ottenere lo stesso risultato senza invocare la funzione sul foglio js "page_edit(but01)" 🤙. 
