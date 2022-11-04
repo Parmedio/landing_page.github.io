2deab3f6f12ca7fc8f8fa7862b960a88fac4010e
Parmedio <111775779+Parmedio@users.noreply.github.com>
2022-11-04 15:35:06 +0100

    update with new js standard

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-var but01 = document.getElementById("col01");
-var but02 = document.getElementById("col02");
-var backGstyle = document.getElementById("current_grad");
-var deg_grad = document.getElementById("deg");
-var css_param = document.getElementsByClassName("centr param_update");
-var col_text = document.getElementsByClassName("txt_inv");
+const but01 = document.getElementById("col01");
+const but02 = document.getElementById("col02");
+const backGstyle = document.getElementById("current_grad");
+const deg_grad = document.getElementById("deg");
+const css_param = document.getElementsByClassName("centr param_update");
+const col_text = document.getElementsByClassName("txt_inv");
 
-function page_edit(variable) {
+const page_edit = (variable) => {
     variable.addEventListener("input", function() {
         a = "linear-gradient(" + Number(deg_grad.value) + "deg, " + but01.value + ", " + but02.value + ") no-repeat fixed";
         backGstyle.style.background = a
         css_param[0].textContent = a
         var list_text = [0, 1, 3, 4];
-        function paint_all(iterable) {
+        const paint_all = (iterable) => {
             for (const x of iterable) {
                 col_text[x].style.color = but01.value
             }
